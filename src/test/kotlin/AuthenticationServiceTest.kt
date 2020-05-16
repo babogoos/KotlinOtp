d384e0f885a7091d853bad46b106bc42154fcbd2
Joey Chen <jay.hatelove@gmail.com>
2020-05-16 17:19:30 +0800

    refactor: extract function in test

--- src/test/kotlin/AuthenticationServiceTest.kt
+++ src/test/kotlin/AuthenticationServiceTest.kt
@@ -5,19 +5,28 @@ import org.junit.Test
 
 class AuthenticationServiceTest {
 
+    private val profile = mockk<ProfileDao>(relaxed = true)
+    private val tokenDao = mockk<RsaTokenDao>(relaxed = true)
+    private val authenticationService = AuthenticationService(profile, tokenDao)
+
     @Test
     fun is_valid() {
-        val profile = mockk<ProfileDao>()
-        every { profile.getPassword("joey") } returns "91"
+        givenPassword("joey", "91")
+        givenToken("000000")
+        shouldBeValid("joey", "91000000")
+    }
 
-        val tokenDao = mockk<RsaTokenDao>()
-        every { tokenDao.getRandom(any()) } returns "000000"
-
-        val authenticationService = AuthenticationService(profile, tokenDao)
-
-        val isValid = authenticationService.isValid("joey", "91000000")
-
+    private fun shouldBeValid(account: String, passcode: String) {
+        val isValid = authenticationService.isValid(account, passcode)
         assertTrue(isValid)
+    }
+
+    private fun givenToken(token: String) {
+        every { tokenDao.getRandom(any()) } returns token
+    }
+
+    private fun givenPassword(account: String, password: String) {
+        every { profile.getPassword(account) } returns password
     }
 }
 
